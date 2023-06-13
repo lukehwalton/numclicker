d32166b44c6900bb46aad0fff9796997226cf209
Luke H. Walton <lukehwalton@icloud.com>
2023-06-13 07:09:45 -0700

    button class completed

--- src/NumButton.jsx
+++ src/NumButton.jsx
@@ -1,13 +1,21 @@
 import {Component} from 'react';
 
 class NumButton extends Component{
-  constructor(num, color){
-    super();
-    this.num = num;
-    this.color = color;
-  }
+  
+  handleClick = () => this.props.onClick(this.props.num);
 
   render(){
-
+    const {num, type} = this.props;
+    const buttonStyle={
+      backgroundColor: 'white',
+      width: '100%',
+      margin: '10px',
+      padding: '10px'
+    }
+    return(
+      <button style={buttonStyle} onClick={this.handleClick} class={`btn btn-outline-${type}`}>{num}</button>
+    );
   }
 }
+
+export default NumButton;
